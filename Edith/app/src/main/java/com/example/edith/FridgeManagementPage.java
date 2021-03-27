aeef9bf333d65a39d97d75832fec353682f7ca45
jovaleri <johnvaleriano98@gmail.com>
2021-03-27 15:45:41 -0500

    Created some pages
    Pages

---
+++ Edith/app/src/main/java/com/example/edith/FridgeManagementPage.java
@@ -0,0 +1,13 @@
+package com.example.edith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FridgeManagementPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fridge_management_page);
+    }
+}
