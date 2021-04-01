65d8c2fc461e5dec2a1ee2294301d1b8426d0221
Saransh Kalra <ksaransh201@gmail.com>
2021-04-01 00:45:03 -0500

    implementing basic navigation drawer

--- Edith/app/src/main/java/com/example/edith/MainActivity.java
+++ Edith/app/src/main/java/com/example/edith/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.edith;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoHome(View view){
+
+        Intent j = new Intent(this, HomeActivity.class);
+        startActivity(j);
+
     }
 }
