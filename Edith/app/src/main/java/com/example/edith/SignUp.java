eb33d625256d2f97495ac2429b54a81a46448556
Saransh Kalra <ksaransh201@gmail.com>
2021-04-01 04:53:41 -0500

    adding a signup page

--- Edith/app/src/main/java/com/example/edith/SignUp.java
+++ Edith/app/src/main/java/com/example/edith/SignUp.java
@@ -6,12 +6,12 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 
     public void gotoHome(View view){
@@ -20,13 +20,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(j);
 
     }
-
-    public void gotoSignUp(View view){
-
-        Intent j = new Intent(this, HomeActivity.class);
-        startActivity(j);
-
-    }
-
-
 }
