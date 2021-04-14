c5d428a12609673f7e2605b60aedfdbd68a8150d
Saransh Kalra <ksaransh201@gmail.com>
2021-04-14 06:49:12 -0500

    Implemented user data register and pic upload
    implemented registering user in database, and uploading photo to firebase storage but havent linked the two and need to get info to populate both the home screen and navigation drawer header

--- Edith/app/src/main/java/com/example/edith/login/SignUp.java
+++ Edith/app/src/main/java/com/example/edith/login/SignUp.java
@@ -1,21 +1,45 @@
 package com.example.edith.login;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.edith.NavDrawerHomeActivity;
 import com.example.edith.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.util.UUID;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText Email, Password;
+    EditText Email, Password, reEnterPassword, DietaryPreference;
+    private ImageView profilePic;
+    private Uri imageUri;
     private FirebaseAuth mAuth;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +48,141 @@ public class SignUp extends AppCompatActivity {
 
         //MainOnCreate
         FirebaseApp.initializeApp(this);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         Email = (EditText) findViewById(R.id.email);
         Password = (EditText) findViewById(R.id.password);
+        reEnterPassword = (EditText) findViewById(R.id.re_enter_pass);
+        DietaryPreference = (EditText) findViewById(R.id.dietary_preference);
+        profilePic = findViewById(R.id.profile_pic);
 
         mAuth = FirebaseAuth.getInstance();
 
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+            }
+            
+        });
+
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData()!= null){
+            imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading Image....");
+        pd.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("images/" + randomKey);
+
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener((new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content), "Image Uploaded", Snackbar.LENGTH_LONG).show();
+                    }
+                }))
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed To Upload", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progressPercent = (100.00 * snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+                        pd.setMessage("Progress: " + (int) progressPercent + "%");
+                    }
+                });
     }
 
     public void registeruser(View view){
 
-        String email = Email.getText().toString();
-        String password = Password.getText().toString();
-        System.out.println("Email --" + email +"Paswd--" + password);
-        mAuth.createUserWithEmailAndPassword(email,password);
+        final String email = Email.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+        String password_match = reEnterPassword.getText().toString().trim();
+        final String dietaryPreference = DietaryPreference.getText().toString().trim();
+
+        if (email.isEmpty()){
+            Email.setError("Email is required!");
+            Email.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Email.setError("Please provide valid email!");
+            Email.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            Password.setError("Password is required!");
+            Password.requestFocus();
+            return;
+        }
+
+        if (password_match.isEmpty()){
+            reEnterPassword.setError("Re-Entering password is required!");
+            reEnterPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 8){
+            Password.setError("Password length should be <= 8!");
+            Password.requestFocus();
+            return;
+        }
+
+        //if (password != password_match) {
+           // Password.setError("Passwords should match!");
+            //reEnterPassword.setError("Passwords should match!");
+           // Password.requestFocus();
+           // return;
+       // }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(email,dietaryPreference);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(SignUp.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        Toast.makeText(SignUp.this, "User has not been registered, Try Again!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
 
         Intent i = new Intent(this, NavDrawerHomeActivity.class);
         startActivity(i);
---
+++ Edith/app/src/main/java/com/example/edith/login/User.java
@@ -0,0 +1,12 @@
+package com.example.edith.login;
+
+public class User {
+
+    public String email, dietary_preference;
+
+    public User(String email, String dietary_preference) {
+        this.email = email;
+        this.dietary_preference = dietary_preference;
+    }
+
+}
